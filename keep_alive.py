091453391f7823d56c72a087cad70f06387bc7b3
Aungkung <97497608+AungS8430@users.noreply.github.com>
2022-01-27 14:47:26 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive" 
+    return "Hi!, I'm Discord bot made with Pycord! Don't forget to add monitor on me(if you are using Repl.it)" 
     
 def run(): 
-    app.run(host='0.0.0.0',port=8080) 
+    app.run(host='0.0.0.0',port=8080) # This is Repl.it default port.
     
 def keep_alive():
     t = Thread(target=run)
